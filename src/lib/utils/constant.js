915be929d01f9c395d643ac4b0302acd11747e9b
271128530@qq.com <mingchao.gao@tonoinfo.com>
2020-10-27 14:54:11 +0800

    修改

--- src/lib/utils/constant.js
+++ src/lib/utils/constant.js
@@ -16,24 +16,25 @@ export function APPFN_SET_VALUE_LABEL(arr) {
   }
   return obj
 }
-
-// 菜单级别
+/**
+ * 菜单级别
+ */
 export const MENU_LEVEL_ARR = [
   {
     value: '1',
     label: '菜单',
-    name: 'MENU',
+    name: 'MENU'
   },
   {
     value: '2',
     label: '功能',
-    name: 'FUN',
+    name: 'FUN'
   },
   {
     value: '3',
     label: '操作',
-    name: 'OPR',
-  },
+    name: 'OPR'
+  }
 ]
 export const MENU_LEVEL_ARR_VL = APPFN_SET_VALUE_LABEL(MENU_LEVEL_ARR)
 
@@ -41,100 +42,104 @@ export const FUCE_BELONG_TYPE = [
   {
     value: 1,
     label: '平台',
-    name: 'platform',
+    name: 'platform'
   },
   {
     value: 2,
     label: '应用',
-    name: 'app',
-  },
+    name: 'app'
+  }
 ]
+
 // 性别
 export const SEX = [
   {
     value: 1,
     label: '男',
-    name: 'man',
+    name: 'man'
   },
   {
     value: 2,
     label: '女',
-    name: 'woman',
+    name: 'woman'
   },
   {
     value: 9,
     label: '未知',
-    name: 'unknown',
-  },
+    name: 'unknown'
+  }
 ]
 export const SEX_VL = APPFN_SET_VALUE_LABEL(SEX)
+export const SEX_NV = APPFN_SET_NAME_VALUE(SEX)
 
 // 数据来源
 export const ROLE_DATA_SCOPE = [
   {
     value: 'none',
     label: '无',
-    name: 'none',
+    name: 'none'
   },
   {
     value: 'all',
     label: '所有',
-    name: 'all',
+    name: 'all'
   },
   {
     value: 'project',
     label: '项目',
-    name: 'project',
+    name: 'project'
   },
   {
     value: 'application',
     label: '应用',
-    name: 'application',
+    name: 'application'
   },
   {
     value: 'site',
     label: '中心',
-    name: 'site',
-  },
+    name: 'site'
+  }
 ]
+export const ROLE_DATA_SCOPE_VL = APPFN_SET_VALUE_LABEL(ROLE_DATA_SCOPE)
+export const ROLE_DATA_SCOPE_NV = APPFN_SET_NAME_VALUE(ROLE_DATA_SCOPE)
 
 // appId
 export const APP_ID = {
   CONFIG: '2597020376823234568', // 配置中心
-  PLATFORM: '2597019974069387266', // 平台
+  PLATFORM: '2597019974069387266' // 平台
 }
 
 export const ORG_TYPE = [
   {
     value: '0',
     label: '系统运维组织',
-    name: 'ADMIN',
+    name: 'ADMIN'
   },
   {
     value: '1',
     label: '研究机构',
-    name: 'RESEARCH_INSTITUTIONS',
+    name: 'RESEARCH_INSTITUTIONS'
   },
   {
     value: '2',
     label: '伦理委员会',
-    name: 'EHTIC',
+    name: 'EHTIC'
   },
   {
     value: '3',
     label: '申办方',
-    name: 'SPONSOR',
+    name: 'SPONSOR'
   },
   {
     value: '4',
     label: 'CRO公司',
-    name: 'CRO',
+    name: 'CRO'
   },
   {
     value: '5',
     label: '研究机构科室',
-    name: 'RESEARCH_INSTITUTIONS_DEPARTMENT',
-  },
+    name: 'RESEARCH_INSTITUTIONS_DEPARTMENT'
+  }
 ]
 // 临床机构单独提出来了，因为用的地方太多了
 export const ORG_RESEARCH_INSTITUTIONS = '1'
@@ -144,13 +149,16 @@ export const ORG_TYPE_NV = APPFN_SET_NAME_VALUE(ORG_TYPE)
 export const ROLE_TTRIAL_CENTER_TYPEYPE = [
   { value: 0, label: '组织管理员', name: 'orgAdmin' },
   { value: 1, label: '应用管理员', name: 'appAdmin' },
-  { value: 2, label: '业务角色', name: 'business' },
+  { value: 2, label: '业务角色', name: 'business' }
 ]
+export const ROLE_TTRIAL_CENTER_TYPEYPE_VL = APPFN_SET_VALUE_LABEL(ROLE_TTRIAL_CENTER_TYPEYPE)
+export const ROLE_TTRIAL_CENTER_TYPEYPE_NV = APPFN_SET_NAME_VALUE(ROLE_TTRIAL_CENTER_TYPEYPE)
 
 // 角色类型
 export const ROLE_TYPE = [
   { value: 0, label: '组织管理员', name: 'orgAdmin' },
   { value: 1, label: '应用管理员', name: 'appAdmin' },
-  { value: 2, label: '业务角色', name: 'business' },
+  { value: 2, label: '业务角色', name: 'business' }
 ]
+export const ROLE_TYPE_VL = APPFN_SET_VALUE_LABEL(ROLE_TYPE)
 export const ROLE_TYPE_NV = APPFN_SET_NAME_VALUE(ROLE_TYPE)
